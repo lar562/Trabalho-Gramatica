45f889586b13e5f2184f5da8084b847720641f67
RafaelSene <RafaelSene@DESKTOP-AQ2I354>
2017-10-01 16:18:25 -0300

    Class Pronta conforme o JavaDoc

--- trab Thiago grupo 01/src/gramatica/AlfabetoGerado.java
+++ trab Thiago grupo 01/src/gramatica/AlfabetoGerado.java
@@ -1,18 +1,36 @@
 package gramatica;
-
+/**
+ * 
+ * @author RafaelSene
+ * Class criada para guarda o alfabeto que pode ser gerado por um caracter especifico.
+ * Ex : X -> abS
+ * 		X -> a
+ * Sendo X o caracter especifico que pode ser substituido por abS ou a para forma a palavra.
+ */
 public class AlfabetoGerado {
 
 	private String alfabeto;
 	
+	/**
+	 * Preenche a variavel com a String passada.
+	 * @param alfabeto recebe a String passada.
+	 */
 	public AlfabetoGerado(String alfabeto) {
-		this.alfabeto = alfabeto;
-		
+		this.alfabeto = alfabeto;		
 	}
-
+	
+	/**
+	 * Retorna a String guardada
+	 * @return
+	 */
 	public String getAlfabeto() {
 		return alfabeto;
 	}
-
+	
+	/**
+	 * Entra com uma String para substituir a que estava guardada.
+	 * @param alfabeto recebe a String passada.
+	 */
 	public void setAlfabeto(String alfabeto) {
 		this.alfabeto = alfabeto;
 	}
