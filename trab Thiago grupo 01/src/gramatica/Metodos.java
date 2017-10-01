6411773ab93f19ab9c62bbac3039de3330ea435e
RafaelSene <RafaelSene@DESKTOP-AQ2I354>
2017-10-01 14:38:42 -0300

    Alterado separarRegraProducao();

--- trab Thiago grupo 01/src/gramatica/Metodos.java
+++ trab Thiago grupo 01/src/gramatica/Metodos.java
@@ -17,8 +17,8 @@ public class Metodos {
 	//	partida = recebeString();
 	//	System.out.print("Entre com os estados terminais os separando por , (Vírgula) : ");
 	//	terminais = recebeString();
-		System.out.print("Entre com regra de producao os separando por / ( Barra ) deixando bem claro a separacao \nExemplo: (S>aSb,S>a,S>a) ou (S/aSb,S/a,S/a): ");
-	//	regraProducao = recebeString();
+		System.out.print("Entre com regra de producao os separando por | ( Barra ) deixando bem claro a separacao \nExemplo: (S|aSb,S|a,S|a): ");
+		separarRegraProducao();
 	//	System.out.print("Entre com palavra a ser computada : ");
 	//	palavra = recebeString();
 	//	separarRegraProducao(regraProducao);
@@ -31,11 +31,13 @@ public class Metodos {
 	
 	public void separarRegraProducao(){
 		
-		for (int i = 0; i < regra.size(); i++) {
-			boolean regraEncontrada = false;
-			String regraProducao;
+		boolean regraEncontrada = false;
+		String regraProducao;
+		do{
 			regraProducao = recebeString();
-			String[] regraGerada = regraProducao.split("/");
+
+			String[] regraGerada = regraProducao.split("");
+			
 			for (int j = 0; j < regra.size(); j++) {
 				if(regra.get(j).getRegra().equals(regraGerada[0])){
 					regra.get(j).addAlfabeto(regraGerada[0]);
@@ -43,13 +45,15 @@ public class Metodos {
 				}
 			}
 			if(!regraEncontrada){
-				//Metodos.regra.ad
-				
+				regra.add
+					
 			}
+			System.out.println("AAAA");
+		}while(regraProducao == "0");
+							
+		System.out.println("BBBB");
+
 				
-		}
-		
-		
 	}
 
 }
