2c1475758a6df709e11be4e5f0b80dadf9cecd5b
Roberto <Roberto@Roberto-PC>
2017-10-01 14:37:26 -0300

    Codigo inicial

--- trab Thiago grupo 01/src/gramatica/Metodos.java
+++ trab Thiago grupo 01/src/gramatica/Metodos.java
@@ -1,71 +1,75 @@
 package gramatica;
-//Roberto 30 cm;
+
 import java.util.Scanner;
 
-/**
- * 
- * @author Joubert Joe Vieira Lellis Essa classe foi criada para organizar todos
- *         os métodos usados para a elaboração do trabalho.
- *
- */
 public class Metodos {
-	/**
-	 * 
-	 * Método reponsavel por partir a string em pedaços e separálos atravéz da
-	 * ultilização de um vetor de Strings.
-	 * 
-	 * @param divisao
-	 *            O que será usado como critério para dividr a String
-	 * @param stringCompleta
-	 *            A Srting a ser dividida
-	 * @return O vetor de Strings já dividido
-	 */
-	public String[] divideString(String divisao, String stringCompleta) {
-		String[] stringDivida = null;
-		stringDivida = stringCompleta.split(divisao);
-		return stringDivida;
+	private static String[] alfabeto;
+	private static String[] terminais;
+	
+	public static void main(String[] args) {
+		
+		
+	recebeAlfabeto();
 	}
 
-	/**
-	 * Método responsável por capturar o que o usuario digita usa um scanner
-	 * para ser possível fazer isso
-	 * 
-	 * @return Uma String com aquilo que foi digitado.
-	 */
-	public String recebeString() {
+		
+		public static String[] divideString(String inputdoUsuario){
+			String aux = inputdoUsuario;
+			
+			String[] dividido = aux.split(",");
+
+			return dividido;
+			
+		}
+		
+
+	
+	public static String recebeAlfabeto(){
+		
 		String inputdoUsuario = null;
-		Scanner input = new Scanner(System.in);
-		inputdoUsuario = input.next();
-		input.close();
+		System.out.println("Digite o alfabeto separando com virgula:");
+		Scanner sc = new Scanner(System.in);
+		inputdoUsuario=sc.next();
+		
+		alfabeto=divideString(inputdoUsuario);
+		recebeTerminais();
 		return inputdoUsuario;
+		
+	}
+	public static String recebeTerminais(){
+		
+		String termin=null;
+		System.out.println("Digite os terminais separando com virgula:");
+		Scanner sc = new Scanner(System.in);
+		termin=sc.next();
+		
+		terminais=divideString(termin);
+		teste();
+		return termin;
+		
+	}
+	public static String regrasTerminais(){
+		String[] regras;
+		
+		System.out.println("Digite as regras de cada terminal :\nSe gerar mais de 2 letras separar com .");
+		Scanner sc = new Scanner(System.in);
+		
+		return null;
+		
+	}
+	
+	public static void teste(){
+		String[] auxA,auxT;
+		auxA=alfabeto;
+		auxT=terminais;
+		int i;
+		for(i=0;i<auxA.length;i++){
+			System.out.print(auxA[i]);
+		}
+		System.out.println();
+		for(i=0;i<auxT.length;i++){
+			System.out.print(auxT[i]);
+		}
 	}
 
-	// Atulizado dia 29/09
-
-	/**
-	 * Usa os métodos de leitura e seperação de Strings para ler o alfabeto
-	 * inserido pelo usuario e o separar em um vetor, atravéz de virgulas.
-	 * 
-	 * @return um vetor de String contendo o Alfabeto
-	 */
-	public String[] lerAlfabeto() {
-		String[] alfabeto = null;
-		System.out.println("Insira os simbolos do alfabeto separados por , (Vírgula) :");
-		String alfabetoInteiro = recebeString();
-		alfabeto = divideString(",", alfabetoInteiro);
-		return alfabeto;
-	}
-
-	/**
-	 * Recebo do usuario os símbolos terminais e as separa dentro de um vetor
-	 * 
-	 * @return Vetor contendo os símbolos terminais
-	 */
-	public String[] lerTerminais() {
-		String[] terminais = null;
-		System.out.println("Insira os simbolos terminais separados por ',' (Vírgula) :");
-		String letrasTerminais = recebeString();
-		terminais = divideString(",", letrasTerminais);
-		return terminais;
-	}
 }
