fa8f41c4b1859e0a2f16f5388689dcfbcbae4cdb
RafaelSene <RafaelSene@DESKTOP-AQ2I354>
2017-10-01 16:48:47 -0300

    Class pronta conforme o JavaDoc

--- trab Thiago grupo 01/src/gramatica/Main.java
+++ trab Thiago grupo 01/src/gramatica/Main.java
@@ -2,6 +2,10 @@ package gramatica;
 
 public class Main {
 
+	/**
+	 * Class principal, funcao unica de chamar a class Metodos.
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		Metodos iniciar = new Metodos();
 
