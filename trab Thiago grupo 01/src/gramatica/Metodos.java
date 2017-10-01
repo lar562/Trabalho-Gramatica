0e731b580f6e2bd9e02138aeba32a2c3ea820cb8
Joubert Joe Vieira Lellis <joubert.lellis@gmail.com>
2017-10-01 14:44:16 -0300

    Concertando a cagada do Rafa
    Concertei a cagada do Rafael

--- trab Thiago grupo 01/src/gramatica/Metodos.java
+++ trab Thiago grupo 01/src/gramatica/Metodos.java
@@ -1,55 +1,71 @@
 package gramatica;
 
-import java.util.ArrayList;
 import java.util.Scanner;
 
+/**
+ * 
+ * @author Joubert Joe Vieira Lellis Essa classe foi criada para organizar todos
+ *         os métodos usados para a elaboração do trabalho.
+ *
+ */
 public class Metodos {
-	
-	private String partida;
-	private String terminais;
-	private String palavra;
-	private ArrayList<Regra> regra;
-	
-	
-	public Metodos() {
-		regra = new ArrayList<Regra>();
-	//	System.out.print("Entre com o ponto de Partida : ");
-	//	partida = recebeString();
-	//	System.out.print("Entre com os estados terminais os separando por , (Vírgula) : ");
-	//	terminais = recebeString();
-		System.out.print("Entre com regra de producao os separando por / ( Barra ) deixando bem claro a separacao \nExemplo: (S>aSb,S>a,S>a) ou (S/aSb,S/a,S/a): ");
-	//	regraProducao = recebeString();
-	//	System.out.print("Entre com palavra a ser computada : ");
-	//	palavra = recebeString();
-	//	separarRegraProducao(regraProducao);
-	}
-	public String recebeString() {
-		Scanner input = new Scanner(System.in);
-		String inputdoUsuario = input.nextLine();		
-		return inputdoUsuario;
-	}
-	
-	public void separarRegraProducao(){
-		
-		for (int i = 0; i < regra.size(); i++) {
-			boolean regraEncontrada = false;
-			String regraProducao;
-			regraProducao = recebeString();
-			String[] regraGerada = regraProducao.split("/");
-			for (int j = 0; j < regra.size(); j++) {
-				if(regra.get(j).getRegra().equals(regraGerada[0])){
-					regra.get(j).addAlfabeto(regraGerada[0]);
-					regraEncontrada = true;
-				}
-			}
-			if(!regraEncontrada){
-				//Metodos.regra.ad
-				
-			}
-				
-		}
-		
-		
+	/**
+	 * 
+	 * Método reponsavel por partir a string em pedaços e separálos atravéz da
+	 * ultilização de um vetor de Strings.
+	 * 
+	 * @param divisao
+	 *            O que será usado como critério para dividr a String
+	 * @param stringCompleta
+	 *            A Srting a ser dividida
+	 * @return O vetor de Strings já dividido
+	 */
+	public String[] divideString(String divisao, String stringCompleta) {
+		String[] stringDivida = null;
+		stringDivida = stringCompleta.split(divisao);
+		return stringDivida;
 	}
 
+	/**
+	 * Método responsável por capturar o que o usuario digita usa um scanner
+	 * para ser possível fazer isso
+	 * 
+	 * @return Uma String com aquilo que foi digitado.
+	 */
+	public String recebeString() {
+		String inputdoUsuario = null;
+		Scanner input = new Scanner(System.in);
+		inputdoUsuario = input.next();
+		input.close();
+		return inputdoUsuario;
+	}
+
+	// Atulizado dia 29/09
+
+	/**
+	 * Usa os métodos de leitura e seperação de Strings para ler o alfabeto
+	 * inserido pelo usuario e o separar em um vetor, atravéz de virgulas.
+	 * 
+	 * @return um vetor de String contendo o Alfabeto
+	 */
+	public String[] lerAlfabeto() {
+		String[] alfabeto = null;
+		System.out.println("Insira os simbolos do alfabeto separados por , (Vírgula) :");
+		String alfabetoInteiro = recebeString();
+		alfabeto = divideString(",", alfabetoInteiro);
+		return alfabeto;
+	}
+
+	/**
+	 * Recebo do usuario os símbolos terminais e as separa dentro de um vetor
+	 * 
+	 * @return Vetor contendo os símbolos terminais
+	 */
+	public String[] lerTerminais() {
+		String[] terminais = null;
+		System.out.println("Insira os simbolos terminais separados por ',' (Vírgula) :");
+		String letrasTerminais = recebeString();
+		terminais = divideString(",", letrasTerminais);
+		return terminais;
+	}
 }
